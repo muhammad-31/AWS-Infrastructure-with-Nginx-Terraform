41f4a9c7b17f4ddc019199e354b07e1b26cae9dc
muhammad <muhammad.binamir31@outlook.com>
2025-06-07 23:38:17 +0500

    v1.0.1

--- main.tf
+++ main.tf
@@ -146,21 +146,7 @@ resource "aws_security_group" "backened-server-sg" {
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port   = 443
-        to_port     = 443
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]             // I will replace 0.0.0.0/0 with the bastion host IP
+        cidr_blocks = ["0.0.0.0/0"]            // I will replace 0.0.0.0/0 with the bastion host IP
     }
 
     ingress {
